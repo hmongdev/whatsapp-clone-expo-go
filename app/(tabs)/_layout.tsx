cf0e7e346da624a1a5e7bc56c23a37e1edd302d5
hmongdev <steveyangwork@gmail.com>
2024-03-12 17:05:27 -0500

    layout styling and page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,45 @@
-import { Text, View } from 'react-native';
+import welcomeImage from '@/assets/images/welcome.png';
+import { Link } from 'expo-router';
+import React from 'react';
+import { Image, Linking, Text, TouchableOpacity, View } from 'react-native';
+const welcome_image = Image.resolveAssetSource(welcomeImage).uri;
 
-export default function Page() {
+export default function WelcomeScreen() {
+	const openLink = () => {
+		Linking.openURL('https://galaxies.dev');
+	};
+
 	return (
-		<View className="flex flex-col grow bg-red-500">
-			<Text className="text-2xl">It works!</Text>
+		<View className="flex h-screen justify-center items-center p-20">
+			<Image
+				source={{ uri: welcome_image }}
+				className="w-full h-[300] rounded-2xl"
+			/>
+			<Text className="text-xl text-center font-bold my-20">
+				Welcome to WhatsApp Clone
+			</Text>
+			<Text className="text-sm text-center mb-20 text-gray-500">
+				Read our{' '}
+				<Text
+					className="text-gray-500"
+					onPress={openLink}>
+					Privacy Policy
+				</Text>
+				. {'Tap "Agree & Continue" to accept the '}
+				<Text
+					className="text-gray-500"
+					onPress={openLink}>
+					Terms of Service
+				</Text>
+				.
+			</Text>
+			<Link href={'/otp'} replace asChild>
+				<TouchableOpacity className="w-full items-center rounded-full py-4 bg-gray-200">
+					<Text className="text-md font-semibold">
+						Agree & Continue
+					</Text>
+				</TouchableOpacity>
+			</Link>
 		</View>
 	);
 }
